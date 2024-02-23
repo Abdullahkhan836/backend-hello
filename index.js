e5705c4f6c8af8c81e06fa27dc5357a5007b7677
Abdullahkhan836 <sk242536366@gmail.com>
2024-02-23 08:56:57 +0500

    backend-hello

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+
+//middleware
+app.use(express.json());
+app.use(cors())
+
+
+const arr = [];
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+// get all users
+app.get('/api/v1/users', (req, res) => {
+    
+    res.send(arr)
+    
+})
+
+//add user
+app.post('/api/v1/users', (req, res) => {
+    const { todo } = req.body;
+    arr.push({
+        todo: todo,
+        id: Date.now(),
+    })
+    res.send('todo added successfully');
+})
+
+//delete user
+app.delete('/api/v1/users/:id', (req, res) => {
+    const { id } = req.params
+    const index = arr.findIndex((user) => user.id === Number(id));
+    if (index === -1) {
+        res.send('user not found');
+        return
+    }
+    arr.splice(index, 1);
+    res.send('user deleted');
+})
+
+// edit user
+app.put('/api/v1/users/:id', (req, res) => {
+    const { editValue } = req.body;
+    const { id } = req.params;
+    const index = arr.findIndex((user) => user.id === Number(id));
+    if (index === -1) {
+        res.send('user not found');
+        return
+    }
+    arr[index].todo = editValue
+    res.send('user edited successfully')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
