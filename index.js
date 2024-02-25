e5b794cdc712cebedab19148d72444626aa648e0
Abdullahkhan836 <sk242536366@gmail.com>
2024-02-25 10:48:07 +0500

    update commads

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT
 
 
 //middleware
@@ -58,5 +59,5 @@ app.put('/api/v1/users/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${process.env.PORT}`);
 })
